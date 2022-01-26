b79c8e43f98d60e02c9d584c58537835125337ff
bqy_fe <1743369777@qq.com>
2022-01-26 21:36:47 +0800

    fix: postcss charset issue

--- vite.config.ts
+++ vite.config.ts
@@ -80,6 +80,21 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
                        @import "src/styles/func.scss";`,
         },
       },
+      // TODO 构建包含@charset问题 https://github.com/vitejs/vite/issues/5833
+      postcss: {
+        plugins: [
+          {
+            postcssPlugin: 'internal:charset-removal',
+            AtRule: {
+              charset: (atRule) => {
+                if (atRule.name === 'charset') {
+                  atRule.remove();
+                }
+              },
+            },
+          },
+        ],
+      },
     },
     server: {
       port: 8888,
